a01f9fe5a6ec2be6202cacd6bcaef46a9c426121
AlphaCyber-X <105292203+AlphaCyber-X@users.noreply.github.com>
2022-05-10 18:28:23 +0530

    Create test3.py

---
+++ test3.py
@@ -0,0 +1,98 @@
+print("\033[2;33;40m\nAlphaCyber-X")
+print()
+print()
+
+
+import time
+from time import sleep
+
+
+a=20
+b=2
+while b<a:
+  print("\033[1;32;40m\nLoading........!!")
+  sleep (0.5)
+  b += 1
+print()
+print()
+print()
+
+import time
+from time import sleep
+
+
+a=5
+b=1
+while b<a:
+     print("\033[3;31;40m\nPlease Wait...............!!")
+     sleep (0.3)
+     b += 1
+print()
+print( "\033[0;36;40m\nSuccessful.........!!" )
+print()
+print()
+print()
+print("\033[0;32;40m\n...........................................................")
+print()
+print()
+
+x=int(input("\033[0;37;40m\nEnter Owner NumberWithout +94 :-"))
+
+import time
+from time import sleep
+x=100
+y=3
+while y<x:
+    print("\033[2;34;40m\nSearching.........................................!!!!")
+    sleep (0.5)
+    y +=10
+print()
+print()
+print("\033[1;37;46m\n...........................................................")
+x=int(input("\033[0;37;40m\nEnter Your Number Without +94 :-"))
+print()
+print(x)
+print()
+print("\033[0;32;40m\nDONE:::::::::!")
+print()
+
+print("\033[0;32;40m\nPlease Wait:::::::::!")
+
+import os
+
+os.system("")
+import os
+import time
+print()
+from time import sleep
+x= 300
+y= 0
+while x>y :
+        print ("\033[0;32;40m\nSearching for the Ascii code", 1+y)
+        sleep (0.3)
+        y+=1
+        if y== 30:
+                print ()
+                print()
+                break
+
+
+x=100
+y=5
+while x>y :
+	sleep(1)
+	y += 5
+	print()
+print("\033[1;37;46m\nCongratulations you passed the test")
+print("\033[0;31;40m\n...........................................................")
+print("\033[0;32;40m\nDo not give this to anyone")
+print("\033[0;31;40m\n...........................................................")
+print("\033[0;32;40m\nGet the link from the ascii code and add it ")
+print("\033[0;31;40m\n...........................................................")
+print("\033[0;32;40m\n104 116 116 112 115 58 47 47 99 104 97 116 46 119 104 97 116 115 97 112 112 46 99 111 109 47 70 55 103 114 65 86 49 111 74 51 89 71 86 108 73 115 118 50 108 90 105 90")
+print()
+print("\033[0;31;40m\n...........................................................")
+print("\033[0;32;40m\nSee you in the groupðŸ˜ƒðŸ˜ƒðŸ˜ƒðŸ˜ƒðŸ˜ƒ")
+print("\033[0;31;40m\n...........................................................")
+print("\033[0;32;40m\nClose the season now")
+print("\033[0;31;40m\n...........................................................")
